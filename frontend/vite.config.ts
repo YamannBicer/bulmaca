4949d946ed1585a990d216718fd2ec6d1be6b1b0
YamanBicer <yamanbicer@gmail.com>
2025-04-02 23:31:51 +0300

    Update deployment to use /app/ subdirectory

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bulmaca/', // Replace with your repository name
+  base: '/bulmaca/app/', // Path includes repository name and app subdirectory
   build: {
     outDir: 'dist',
     assetsDir: 'assets',

7a3edba60d904d3ff432b2828c82b31ea23966f0
YamanBicer <yamanbicer@gmail.com>
2025-04-03 00:51:29 +0300

    Simplify to just show a static photo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
-import PuzzleGame from './components/PuzzleGame'
 
 function App() {
+  // Base path will be different in development vs production
+  const basePath = import.meta.env.BASE_URL || '';
+  const photoPath = `${basePath}/photos/sample1.jpg`;
+
   return (
     <div className="app-container">
-      <PuzzleGame />
+      <h1>My Photo</h1>
+      <div className="photo-container">
+        <img src={photoPath} alt="My Photo" className="photo" />
+      </div>
     </div>
   )
 }
